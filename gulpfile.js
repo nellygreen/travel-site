4c29a7b0c5c4bbc730af7aec5b2349414a54e7b0
nellygreen <nellygreen.cg@gmail.com>
2016-10-12 17:06:34 +0300

    Configured workflow main-css to temp-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require ('gulp');
 var watch = require ('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
 
 gulp.task('default', function() {
 	console.log('This is a default gulp task.');
@@ -8,10 +13,6 @@ gulp.task('default', function() {
 gulp.task('html', function() {
 	console.log('Html changes here.');
 });
-
-gulp.task('styles', function() {
-	console.log('CSS changes here.');
-})
 
 gulp.task('watch', function() {
 	watch('./app/index.html', function() {
@@ -22,3 +23,9 @@ gulp.task('watch', function() {
 		gulp.start('styles');
 	});
 });
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(gulp.dest('./app/temp/styles'))
+	.pipe(postcss([autoprefixer,cssvars,nested]))
+})
