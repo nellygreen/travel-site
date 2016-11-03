f6b2d8f1d516d0c73e7d4cfa71dcb0554cf73bc9
nellygreen <nellygreencg@mail.com>
2016-11-03 18:36:14 +0300

    Finished Modal

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,44 @@
+import $ from 'jquery';
+
+class Modal {
+	constructor() {
+		this.openModalButton = $(".open-modal");
+		this.modal = $(".modal");
+		this.closeModalButton = $(".modal__close");
+		this.events();
+	};
+	
+	events() {
+		//clicking the open-button
+		
+		this.openModalButton.click(this.openModal.bind(this));
+		
+		//clicking the close-button
+		
+		this.closeModalButton.click(this.closeModal.bind(this));
+		
+		//clicking ESC-button
+		$(document).keyup(this.keyPressHandler.bind(this));
+		
+	};
+	
+	openModal() {
+		this.modal.addClass("modal--is-visible");
+		return false;
+	};
+	
+	closeModal() {
+		this.modal.removeClass("modal--is-visible");
+	};
+	
+	keyPressHandler(e) {
+		if(e.keyCode == 27) {
+			this.closeModal();
+		}
+	}
+	
+	
+	
+}
+
+export default Modal;
